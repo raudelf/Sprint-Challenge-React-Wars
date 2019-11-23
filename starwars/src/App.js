1008163140e3000cc55ce3c335859fd5e463ced6
Raudel Flores <rawdell@outlook.com>
2019-11-22 22:07:14 -0600

    Completed MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Card from './components/Card';
+import styles from 'styled-components';
 
 const App = () => {
   
@@ -10,18 +12,36 @@ const App = () => {
     axios.get('https://swapi.co/api/people/')
     .then(res => {
       console.log(res);
-      return setPerson(res);
+      return setPerson(res.data.results);
     })
     .catch(err => {
       return `Error: ${err}`;
     })
   };
+  useEffect(fetchUser, []);
 
-  
+  const Container = styles.div`
+  margin: 0 auto;
+  width: 1000px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  `;
+
+  const Headline = styles.div`
+  width: 100%;
+  text-align: center;
+  `;
+
   return (
-    <div className="App">
+    <Container className="App">
+      <Headline>
       <h1 className="Header">React Wars</h1>
-    </div>
+      </Headline>
+      {person.map((value, index) => {
+        return <Card name={value.name} gender={value.gender} birth={value.birth_year} hair={value.hair_color} eyes={value.eye_color} height={value.height} mass={value.mass} key={index}/>
+      })}
+    </Container>
   );
 }
 
