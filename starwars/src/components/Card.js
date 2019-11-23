52c08511a855fd14dab50927ab3ea1919525945b
Raudel Flores <rawdell@outlook.com>
2019-11-22 22:06:57 -0600

    added styles for the cards

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,19 +1,50 @@
 import React from 'react';
+import styles from 'styled-components';
 
+const PersonCard = styles.div`
+width: 450px;
+margin: 10px;
+padding: 10px
+background-color: white;
+display: flex;
+flex-direction: column;
+border-radius: 5px;
+`;
+
+const Name = styles.div`
+width: 100%;
+text-align: center;
+`;
+
+const List = styles.ul`
+width: 100%;
+display: flex;
+flex-direction: column;
+list-style-type: none;
+align-items: center;
+justify-content: center;
+`;
+
+const Item = styles.li`
+width: 100%;
+text-align: left;
+`;
 const Card = props => {
     return (
-        <div classname='person-card'>
+        <PersonCard className='person-card'>
+        <Name>
         <h1>{props.name}</h1>
-        <h3>Gender: {props.gender} Born: {props.birth} </h3>
-        <h3>From {props.homeworld}</h3>
-        <ul>
-            <li>Hair Color: {props.hair}</li>
-            <li>Eye Color: {props.eyes}</li>
-            <li>Height: {props.height}</li>
-            <li>Mass: {props.mass}</li>
-        </ul>
+        <h3>Born: {props.birth}</h3>
+        </Name>
+        <List>
+            <Item><strong>Gender:</strong> {props.gender}</Item>
+            <Item><strong>Hair Color:</strong> {props.hair}</Item>
+            <Item><strong>Eye Color:</strong> {props.eyes}</Item>
+            <Item><strong>Height:</strong> {props.height}</Item>
+            <Item><strong>Mass:</strong> {props.mass}</Item>
+        </List>
 
-        </div>
+        </PersonCard>
     )
 }
 
